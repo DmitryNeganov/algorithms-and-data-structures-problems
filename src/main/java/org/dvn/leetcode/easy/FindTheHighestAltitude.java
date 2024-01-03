770f6418d0665044793dd9cb821ee55398c694ba
DmitryNeganov <glorynewfound@yandex.ru>
2024-01-03 23:35:31 +0300

    1732. Find the Highest Altitude

---
+++ src/main/java/org/dvn/leetcode/easy/FindTheHighestAltitude.java
@@ -0,0 +1,16 @@
+package org.dvn.leetcode.easy;
+
+//1732
+public class FindTheHighestAltitude {
+    public int largestAltitude(int[] gain) {
+        int maxAlt = 0;
+        int currentAlt = 0;
+        for (int i = 0; i < gain.length; i++) {
+            currentAlt += gain[i];
+            if (currentAlt > maxAlt) {
+                maxAlt = currentAlt;
+            }
+        }
+        return maxAlt;
+    }
+}
