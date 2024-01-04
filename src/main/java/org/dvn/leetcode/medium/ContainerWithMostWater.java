275d603b8987ee029a463239bd99e6daa58bcc5d
DmitryNeganov <glorynewfound@yandex.ru>
2024-01-04 14:05:33 +0300

    11. Container With Most Water

---
+++ src/main/java/org/dvn/leetcode/medium/ContainerWithMostWater.java
@@ -0,0 +1,23 @@
+package org.dvn.leetcode.medium;
+
+//11
+public class ContainerWithMostWater {
+    public int maxArea(int[] height) {
+        int left = 0, right = height.length - 1;
+        int maxVol = 0;
+
+        while (left < right) {
+            int width = right - left;
+            int vol = Math.min(height[left], height[right]) * width;
+            if (vol > maxVol) {
+                maxVol = vol;
+            }
+            if (height[left] > height[right]) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return maxVol;
+    }
+}
