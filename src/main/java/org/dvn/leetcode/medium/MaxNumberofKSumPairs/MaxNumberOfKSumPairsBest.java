47a7c20255eff301deab5512854e46dddac013d8
DmitryNeganov <glorynewfound@yandex.ru>
2024-01-05 17:41:59 +0300

    1679. Max Number of K-Sum Pairs - best solution O(N)

---
+++ src/main/java/org/dvn/leetcode/medium/MaxNumberofKSumPairs/MaxNumberOfKSumPairsBest.java
@@ -0,0 +1,33 @@
+package org.dvn.leetcode.medium.MaxNumberofKSumPairs;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//1679
+public class MaxNumberOfKSumPairsBest {
+
+    public int maxOperations(int[] nums, int k) {
+        int operations = 0;
+        Map<Integer, Integer> mapa = new HashMap<>();
+        for (int i: nums) {
+            int searchingValue = k - i;
+            if (mapa.containsKey(searchingValue)) {
+                int quantity = mapa.get(searchingValue);
+                if (quantity - 1 == 0) {
+                    mapa.remove(searchingValue);
+                } else {
+                    mapa.replace(searchingValue, quantity, quantity - 1);
+                }
+                operations++;
+            } else {
+                if (mapa.containsKey(i)) {
+                    mapa.put(i, mapa.get(i) + 1);
+                } else {
+                    mapa.put(i, 1);
+                }
+
+            }
+        }
+        return operations;
+    }
+}
