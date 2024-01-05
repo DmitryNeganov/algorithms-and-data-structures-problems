5fe8bae0fd28b715ab605327a0d0e0d32524cbe7
DmitryNeganov <glorynewfound@yandex.ru>
2024-01-05 17:19:25 +0300

    1679. Max Number of K-Sum Pairs - sort first O(NlogN)

---
+++ src/main/java/org/dvn/leetcode/medium/MaxNumberofKSumPairs/MaxNumberOfKSumPairsSortFirst.java
@@ -0,0 +1,26 @@
+package org.dvn.leetcode.medium.MaxNumberofKSumPairs;
+
+import java.util.Arrays;
+
+//1679
+public class MaxNumberOfKSumPairsSortFirst {
+
+    public int maxOperations(int[] nums, int k) {
+        int operations = 0;
+        Arrays.sort(nums);
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            if (sum == k) {
+                operations++;
+                left++;
+                right--;
+            } else if (sum > k) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return operations;
+    }
+}
