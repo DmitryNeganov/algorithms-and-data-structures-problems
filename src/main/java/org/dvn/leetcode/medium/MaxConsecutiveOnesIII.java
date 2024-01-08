8067857ff07e97e95ccaaa697bfcef27f19eee3c
DmitryNeganov <glorynewfound@yandex.ru>
2024-01-08 13:04:55 +0300

    1004. Max Consecutive Ones III

---
+++ src/main/java/org/dvn/leetcode/medium/MaxConsecutiveOnesIII.java
@@ -0,0 +1,28 @@
+package org.dvn.leetcode.medium;
+
+//1004
+public class MaxConsecutiveOnesIII {
+    public int longestOnes(int[] nums, int k) {
+        int left = 0;
+        int zeros = 0;
+        int maxWindowWithKZeros = 0;
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] == 0) {
+                zeros++;
+            }
+            while (zeros > k) {
+                if (nums[left] == 0) {
+                    zeros--;
+                }
+                left++;
+            }
+            int window = right - (left - 1);
+            if (maxWindowWithKZeros < window) {
+                maxWindowWithKZeros = window;
+            }
+        }
+        return maxWindowWithKZeros;
+    }
+}
+
+
