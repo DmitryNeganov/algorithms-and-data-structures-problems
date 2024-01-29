a6e6b2fd0be7e420a1014ebf25909190342ba3d8
DmitryNeganov <glorynewfound@yandex.ru>
2024-01-29 17:25:43 +0300

    206. Reverse Linked List

---
+++ src/main/java/org/dvn/leetcode/easy/linked_list/ReverseLinkedList.java
@@ -0,0 +1,34 @@
+package org.dvn.leetcode.easy.linked_list;
+
+//206
+public class ReverseLinkedList {
+
+    public ListNode reverseList(ListNode head) {
+        ListNode current = head;
+        ListNode prev = null;
+        while (current != null) {
+            ListNode next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
