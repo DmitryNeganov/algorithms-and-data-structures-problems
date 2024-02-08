b05e6f550856e1254a34f74010e061d539f3b051
DmitryNeganov <glorynewfound@yandex.ru>
2024-02-08 11:48:30 +0300

    104. Maximum Depth of Binary Tree

---
+++ src/main/java/org/dvn/leetcode/easy/binarytree/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,34 @@
+package org.dvn.leetcode.easy.binarytree;
+
+import javax.swing.tree.TreeNode;
+
+//104
+public class MaximumDepthOfBinaryTree {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return Math.max(left, right) + 1;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
