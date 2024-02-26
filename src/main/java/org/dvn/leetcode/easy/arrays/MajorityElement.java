e0b9ce495fa5b87ce5888b2d99f8a3ae02494588
DmitryNeganov <glorynewfound@yandex.ru>
2024-02-27 00:03:15 +0300

    169. Majority Element

---
+++ src/main/java/org/dvn/leetcode/easy/arrays/MajorityElement.java
@@ -0,0 +1,22 @@
+package org.dvn.leetcode.easy.arrays;
+
+//169
+public class MajorityElement {
+
+    public int majorityElement(int[] nums) {
+        int element = -1;
+        int counter = 0;
+
+        for (int i : nums) {
+            if (counter == 0) {
+                element = i;
+                counter++;
+            } else if (element == i) {
+                counter++;
+            } else {
+                counter--;
+            }
+        }
+        return element;
+    }
+}
