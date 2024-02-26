c364f7898fb33d242705ff7e040781c97e508e1e
DmitryNeganov <glorynewfound@yandex.ru>
2024-02-26 19:14:04 +0300

    986. Interval List Intersections

---
+++ src/main/java/org/dvn/leetcode/easy/arrays/IntervalListIntersections.java
@@ -0,0 +1,27 @@
+package org.dvn.leetcode.easy.arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//986
+public class IntervalListIntersections {
+    public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
+        List<int[]> list = new ArrayList<>();
+        int i = 0, j = 0;
+        while (i < firstList.length && j < secondList.length) {
+            int startMax = Math.max(firstList[i][0], secondList[j][0]);
+            int endMin = Math.min(firstList[i][1], secondList[j][1]);
+
+            if (startMax <= endMin) {
+                list.add(new int[]{startMax, endMin});
+            }
+
+            if (firstList[i][1] <= secondList[j][1]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+        return list.toArray(new int[list.size()][2]);
+    }
+}
