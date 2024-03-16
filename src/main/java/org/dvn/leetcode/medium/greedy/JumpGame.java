e293898019afeb4729dd121c5fd05db0878d71ff
DmitryNeganov <glorynewfound@yandex.ru>
2024-03-16 18:42:18 +0300

    55. Jump Game

---
+++ src/main/java/org/dvn/leetcode/medium/greedy/JumpGame.java
@@ -0,0 +1,16 @@
+package org.dvn.leetcode.medium.greedy;
+
+//55. Jump Game
+public class JumpGame {
+    public boolean canJump(int[] nums) {
+        int reachable = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (reachable < i) {
+                return false;
+            }
+            reachable = Math.max(reachable, i + nums[i]);
+        }
+        return true;
+    }
+}
