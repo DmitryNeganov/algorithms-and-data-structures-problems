90c390c2b1d5673cfd9bc5b08c376fafb706acda
DmitryNeganov <glorynewfound@yandex.ru>
2024-03-17 17:15:36 +0300

    1306. Jump Game III

--- src/main/java/org/dvn/leetcode/medium/greedy/JumpGameIII.java
+++ src/main/java/org/dvn/leetcode/medium/greedy/JumpGameIII.java
@@ -2,4 +2,13 @@ package org.dvn.leetcode.medium.greedy;
 
 //1306. Jump Game III
 public class JumpGameIII {
+    public boolean canReach(int[] arr, int start) {
+        if (start > arr.length - 1 || start < 0 || arr[start] == -1) return false;
+        if (arr[start] == 0) return true;
+
+        int temp = arr[start];
+        arr[start] = -1;
+
+        return canReach(arr, start - temp) || canReach(arr, start + temp);
+    }
 }
