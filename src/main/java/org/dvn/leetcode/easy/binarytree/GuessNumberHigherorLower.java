1adc45a99957598b4d49c181e19d8295ebc37950
DmitryNeganov <glorynewfound@yandex.ru>
2024-03-24 23:12:36 +0300

    374. Guess Number Higher or Lower

--- src/main/java/org/dvn/leetcode/easy/binarytree/GuessNumberHigherorLower.java
+++ src/main/java/org/dvn/leetcode/easy/binarytree/GuessNumberHigherorLower.java
@@ -2,4 +2,25 @@ package org.dvn.leetcode.easy.binarytree;
 
 //374. Guess Number Higher or Lower
 public class GuessNumberHigherorLower {
+
+    public int guess(int num) {
+        return 0;
+    }
+    public int guessNumber(int n) {
+        int first = 1, last = n;
+
+        int current = first + (last - first) / 2;
+        int result = guess(current);
+        while (result != 0) {
+            if (result < 0) {
+                last = current - 1;
+            } else {
+                first = current + 1;
+            }
+            current = first + (last - first) / 2;
+            result = guess(current);
+        }
+
+        return current;
+    }
 }
